8631a41888c4ad8d7266cd1b4078fc1b9ba94d5c
Matthewtrainsgaming <matthew_701@msn.com>
2023-04-12 15:15:23 +0000

    stuff lol

--- script.js
+++ script.js
@@ -2,18 +2,32 @@ const itBtn = document.getElementById('it')
 const sdpBtn = document.getElementById('sdp')
 const classesDiv = document.getElementById("classes");
 
+itBtn.addEventListener()
 
+function itClick() {
+  const testData = [
+    "How",
+    "Ethical Hacking",
+    "Cloud and Devops",
+    "VOIP",
+    "Capstone Project",
+    "Webserver Admin",
+    "San and desater recovery",
+    "Test",
+    "Test",
+    "Test",
+    "Test",
+    "Test",
+    "Test"
+  ];
 
-
-
-function itClick(){
   // Convert the test data into an HTML string
-const classesHtml = testData.map(text => `<a class="link" target="_blank">${text}</a>`).join("");
+  const classesHtml = testData.map(text => `<a class="link" target="_blank">${text}</a>`).join("");
 
-// Set the innerHTML of the "classes" div to the HTML string
-classesDiv.innerHTML = classesHtml;
+  // Set the innerHTML of the "classes" div to the HTML string
+  classesDiv.innerHTML = classesHtml;
 }
 
-function sdpClick(){
- 
+function sdpClick() {
+
 }
