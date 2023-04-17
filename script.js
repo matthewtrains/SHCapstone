a804308c4d06799f53344a4c674496317879af8b
Matthewtrainsgaming <matthew_701@msn.com>
2023-04-17 14:48:19 +0000

    Stuff

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 document.getElementById('it').addEventListener('click', itClick);
 document.getElementById('sdp').addEventListener('click', sdpClick);
 const classesDiv = document.getElementById("classes");
-const classesDiv = document.getElementById("classes");
+const detailsDiv = document.getElementById("details");
 
 
 function itClick() {
+  classesDiv.innerHTML = "";
+
   const testData = [
     "How",
     "Ethical Hacking",
@@ -26,26 +28,45 @@ function itClick() {
 
   // Set the innerHTML of the "classes" div to the HTML string
   classesDiv.innerHTML = classesHtml;
+
+  // Add a click event listener to each detail element
+  const detailElements = document.querySelectorAll(".link");
+  detailElements.forEach((element, index) => {
+    element.addEventListener("click", () => {
+      // Set the innerHTML of the "details" div to the appropriate HTML string
+      detailsDiv.innerHTML = `<h2>${testData[index]}</h2><p>Details for ${testData[index]}</p>`;
+    });
+  });
 }
 
 function sdpClick() {
+  classesDiv.innerHTML = "";
+
   const testData = [
-    "Beep",
-    "Boop",
-    "Beep",
-    "Boop",
-    "Beep",
-    "Boop",
-    "Beep",
-    "Boop",
-    "Beep",
-    "Boop",
-    "Beep",
-    "Boop",
-    "Beep",
-    "Boop",
-    "Beep",
-    "Boop",
+    "This",
+    "Is",
+    "To",
+    "Test",
+    "This",
+    "Function",
+    "This",
+    "Is",
+    "To",
+    "Test",
+    "This",
+    "Function",
+    "This",
+    "Is",
+    "To",
+    "Test",
+    "This",
+    "Function",
+    "This",
+    "Is",
+    "To",
+    "Test",
+    "This",
+    "Function"
   ];
 
   // Convert the test data into an HTML string
@@ -53,4 +74,13 @@ function sdpClick() {
 
   // Set the innerHTML of the "classes" div to the HTML string
   classesDiv.innerHTML = classesHtml;
+
+  // Add a click event listener to each detail element
+  const detailElements = document.querySelectorAll(".link");
+  detailElements.forEach((element, index) => {
+    element.addEventListener("click", () => {
+      // Set the innerHTML of the "details" div to the appropriate HTML string
+      detailsDiv.innerHTML = `<h2>${testData[index]}</h2><p>Details for ${testData[index]}</p>`;
+    });
+  });
 }
